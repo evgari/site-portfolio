015f7b3be935c8ab1199d708bb2c08b886e4fa2e
Evgeniy Oshepkov <antarievg@yandex.ru>
2023-11-28 12:46:27 +0300

    all section complite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import 'normalize.css';
 import './styles/main.scss';
 
+const anchors = document.querySelectorAll('.js-scroll-to');
+
+const select = () => {
+  const element = document.querySelector('.js-choice');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    shouldSort: false,
+    itemSelectText: '',
+    position: 'bottom',
+  });
+};
+
+select();
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const blockID = anchor.getAttribute('href');
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
+
 
 
 
