b89e1d49890a1803fedae7b8beec860bbb959c58
Evgeniy Oshepkov <antarievg@yandex.ru>
2023-11-30 21:38:29 +0300

    add functions disable scroll

--- src/index.js
+++ src/index.js
@@ -55,12 +55,39 @@ const modalController = ({modal, btnOpen, btnClose}) => {
       setTimeout(() => {
         modalElem.style.visibility = 'hidden';
       }, 300);
+
+      enabledScroll();
     }
   };
   
   const openModal = () => {
     modalElem.style.visibility = 'visible';
     modalElem.style.opacity = '1';
+
+    disabledScroll();
+  };
+
+  const disabledScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+  
+    document.body.dbScrollY = window.scrollY;
+  
+    document.body.style.cssText = `
+      position: fixed;
+      top: ${-window.scrollY}px;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      overflow: hidden;
+      padding-right: ${widthScroll}px;
+    `;
+  };
+  
+  const enabledScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+      top: document.body.dbScrollY,
+    })
   };
   
   modalBtns.forEach(btn => {
